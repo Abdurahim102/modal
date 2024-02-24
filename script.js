76fe09832b92de017da675bfb55e2ef3574bc287
Abdurahim102 <abundance0121@gmail.com>
2024-02-24 21:09:57 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let btn = document.querySelector(".btn")
+let card = document.querySelector(".card")
+let btn2 = document.querySelector(".btn2")
+
+btn.addEventListener("click", ()=>{
+    card.style.top = "50px"
+})
+
+btn2.addEventListener("click", ()=>{
+    card.style.top = "-100%"
+})
+color1.setTimeout(() => {
+    
+}, timeout);
